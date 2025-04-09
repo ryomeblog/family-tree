a873e4e65bade5030420b006862ad27d7e0dc356
ryome <ryome.blog@gmail.com>
2025-04-10 08:51:16 +0900

    Updates

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+import '../src/index.css';
+import React from 'react';
+import 'reactflow/dist/style.css';
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
